777d0a6372c06e5665bf102402df80ac95c6aaf1
Eliezer Almeida <eliezeralmeida94@hotmail.com>
2026-01-09 18:53:05 -0300

    Adição de testes unitários com JUnit para VendaService

---
+++ test/br/com/palavraviva/service/test/VendaServiceTest.java
@@ -0,0 +1,21 @@
+package br.com.palavraviva.service.test;
+
+import br.com.palavraviva.service.VendaService;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class VendaServiceTest {
+
+    @Test
+    public void deveCalcularTotalCorretamente() {
+        VendaService service = new VendaService(null, null, null);
+        double total = service.calcularTotal(50.0, 2);
+        assertEquals(100.0, total, 0.001);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void deveLancarErroParaQuantidadeInvalida() {
+        VendaService service = new VendaService(null, null, null);
+        service.calcularTotal(50.0, 0);
+    }
+}
